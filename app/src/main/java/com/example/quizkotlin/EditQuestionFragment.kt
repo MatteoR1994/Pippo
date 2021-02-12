5c6bcefae5de0b7ac95276bf8adffd8a64e8407e
Matteo Rinaldi <matteo.rinaldi1893@gmail.com>
2021-02-12 17:38:27 +0100

    Terminata implementazione della modifica di una domanda.

--- app/src/main/java/com/example/quizkotlin/EditQuestionFragment.kt
+++ app/src/main/java/com/example/quizkotlin/EditQuestionFragment.kt
@@ -1,21 +1,88 @@
 package com.example.quizkotlin
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.example.quizkotlin.DatabaseManager.QuestionEntry.COLUMN_OPZIONE1
+import com.example.quizkotlin.DatabaseManager.QuestionEntry.COLUMN_OPZIONE2
+import com.example.quizkotlin.DatabaseManager.QuestionEntry.COLUMN_OPZIONE3
+import com.example.quizkotlin.DatabaseManager.QuestionEntry.COLUMN_RISPOSTA
+import com.example.quizkotlin.DatabaseManager.QuestionEntry.COLUMN_TESTO
+import com.example.quizkotlin.DatabaseManager.QuestionEntry.TABLE_NAME
+import kotlinx.android.synthetic.main.fragment_edit_question.*
 
-
+// Questo è il fragment che permette di modificare una domanda.
 class EditQuestionFragment : Fragment() {
-
+    // Imposto, come sempre, le variabili di cui avremo bisogno.
+    lateinit var editedQuestionText: EditText
+    lateinit var editedOptionAnswer1: EditText
+    lateinit var editedOptionAnswer2: EditText
+    lateinit var editedOptionAnswer3: EditText
+    lateinit var editedRightAnswer: EditText
+    lateinit var updateQuestionButton: Button
+    var txtQ: String? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-         val txtQ = arguments?.getString("question")
-        // Inflate the layout for this fragment
-      val v= inflater.inflate(R.layout.fragment_edit_question, container, false)
+        val v= inflater.inflate(R.layout.fragment_edit_question, container, false)
+        editedQuestionText = v.findViewById(R.id.editedQuestionText)
+        editedOptionAnswer1 = v.findViewById(R.id.editedOptionAnswer1)
+        editedOptionAnswer2 = v.findViewById(R.id.editedOptionAnswer2)
+        editedOptionAnswer3 = v.findViewById(R.id.editedOptionAnswer3)
+        editedRightAnswer = v.findViewById(R.id.editedRightAnswer)
+        updateQuestionButton = v.findViewById(R.id.editQuestionButton)
+        txtQ = arguments?.getString("question") // Recupero il parametro passato al fragment
+        // Seleziono tutti i dati necessari alla domanda (testo, 3 opzioni di risposta e risposta esatta).
+        val mDBHelper = QuestionDBHelper(requireContext())
+        val db = mDBHelper.readableDatabase
+        val projection = arrayOf(COLUMN_TESTO, COLUMN_OPZIONE1, COLUMN_OPZIONE2, COLUMN_OPZIONE3, COLUMN_RISPOSTA)
+        val selection = "$COLUMN_TESTO = ?"
+        val selectionArgs = arrayOf("$txtQ")
+        val cursor : Cursor = db.query(TABLE_NAME, projection, selection, selectionArgs, null, null , null)
+        val questionTextColumnIndex = cursor.getColumnIndexOrThrow(COLUMN_TESTO)
+        val option1ColumnIndex = cursor.getColumnIndexOrThrow(COLUMN_OPZIONE1)
+        val option2ColumnIndex = cursor.getColumnIndexOrThrow(COLUMN_OPZIONE2)
+        val option3ColumnIndex = cursor.getColumnIndexOrThrow(COLUMN_OPZIONE3)
+        val rightAnswerColumnIndex = cursor.getColumnIndexOrThrow(COLUMN_RISPOSTA)
+        while(cursor.moveToNext()) {
+            val newQuestionString = cursor.getString(questionTextColumnIndex)
+            val newOption1String = cursor.getString(option1ColumnIndex)
+            val newOption2String = cursor.getString(option2ColumnIndex)
+            val newOption3String = cursor.getString(option3ColumnIndex)
+            val newRightAnswer = cursor.getInt(rightAnswerColumnIndex)
+            // Imposto il testo dei campi input EditText (presenti in fragment_edit_question.xml) con i valori corrispondenti
+            // selezionati dal database in base alla domanda passata, per poterli modificare.
+            editedQuestionText.setText(newQuestionString)
+            editedOptionAnswer1.setText(newOption1String)
+            editedOptionAnswer2.setText(newOption2String)
+            editedOptionAnswer3.setText(newOption3String)
+            editedRightAnswer.setText(newRightAnswer.toString())
+        }
+        cursor.close()
+        // Alla pressione del bottone per modificare...
+        updateQuestionButton.setOnClickListener {
+            val db = mDBHelper.writableDatabase
+            // Imposto, come sempre, i valori da inserire con i nuovi input prelevati dal form nel layout.
+            val values = ContentValues().apply {
+                put(COLUMN_TESTO, editedQuestionText.text.toString())
+                put(COLUMN_OPZIONE1, editedOptionAnswer1.text.toString())
+                put(COLUMN_OPZIONE2, editedOptionAnswer2.text.toString())
+                put(COLUMN_OPZIONE3, editedOptionAnswer3.text.toString())
+                put(COLUMN_RISPOSTA, editedRightAnswer.text.toString().toInt())
+            }
+            val selectionArgs2 = arrayOf("$txtQ")
+            // Eseguo la query di update nel database:
+            // Aggiorna i valori "values" nella tabella "TABLE_NAME" dove il testo della domanda è uguale a "selectionArgs2"
+            val rowId = db.update(TABLE_NAME, values, "$COLUMN_TESTO = ?", selectionArgs2)
+            // A query eseguita, torno al fragment della lista delle domande. Se tutto va liscio, quando lo riapirà la lista si aggiornerà da sola.
+            v.findNavController().navigate(R.id.action_editQuestionFragment_to_listQuestionsFragment)
+        }
         return v
     }
-
-
-
 }
--- app/src/main/java/com/example/quizkotlin/QuestionAdapter.kt
+++ app/src/main/java/com/example/quizkotlin/QuestionAdapter.kt
@@ -15,10 +15,8 @@ import com.example.quizkotlin.DatabaseManager.QuestionEntry._ID
 import kotlinx.android.synthetic.main.question_details_layout.view.*
 
 class QuestionAdapter(private var questionList: MutableList<Question>) : RecyclerView.Adapter<QuestionAdapter.QuestionViewHolder>() {
-
-    class QuestionViewHolder(v: View) : RecyclerView.ViewHolder(v),View.OnClickListener{
-
-
+    class QuestionViewHolder(v: View) : RecyclerView.ViewHolder(v),View.OnClickListener{ // ",View.OnClickListener" va messo per poter rendere
+        // cliccabile l'elemento della lista.
         private lateinit var question: Question
         private var view: View
         private var questionText: TextView
@@ -26,40 +24,38 @@ class QuestionAdapter(private var questionList: MutableList<Question>) : Recycle
         private var optionAnswer2: TextView
         private var optionAnswer3: TextView
         init {
-
             view = v
-            v.setOnClickListener(this)
+            v.setOnClickListener(this) // Questo indica che la nostra v (quindi l'elemento della lista) avrà settato su se stessa un metodo
+            // onClickListener, per poter implementare il metodo onClick.
             questionText = view.findViewById(R.id.questionTextItem)
-
+            optionAnswer1 = view.findViewById(R.id.optionAnswer1Item)
             optionAnswer2 = view.findViewById(R.id.optionAnswer2Item)
             optionAnswer3 = view.findViewById(R.id.optionAnswer3Item)
-            optionAnswer1 = view.findViewById(R.id.optionAnswer1Item)
         }
-
         fun bindQuestion(parent: QuestionAdapter  , question: Question, position: Int) {
-
-            view.deleteBtn.setOnClickListener {
-
+            view.deleteBtn.setOnClickListener { // Il listener del bottone abbiamo dovuto metterlo qua (in realtà
+                // dovrebbe essere dentro a "onCreateViewHolder") perché avevamo necessità della posizione dell'elemento
+                // nella lista, per fare il removeAt e per il valore da passare nella query.
                 val mDBHelper = QuestionDBHelper(view.context)
-
                 val db = mDBHelper.writableDatabase
-
-                val selection = "${COLUMN_TESTO} = ?"
-                val selectionArgs = arrayOf( "${parent.questionList[position].question}")
-
+                val selection = "${COLUMN_TESTO} = ?" // Come condizione del WHERE abbiamo usato il testo della domanda (che poi
+                // è passato anche per la modifica, vedi sotto) perché quando abbiamo costruito il database non abbiamo previsto la
+                // presenza dell'id per ogni riga.
+                val selectionArgs = arrayOf( "${parent.questionList[position].question}") // Questo permette di prendere la lista passata in input
+                // all'adapter, come detto in "onBindViewHolder". Perché da dentro a questa classe interna non potevamo accedere a "questionList".
                 db.delete(TABLE_NAME, selection, selectionArgs)
-
                 parent.questionList.removeAt(position)
-
                 parent.notifyDataSetChanged()
-
-
             }
             this.question = question
             this.questionText.text = question.question
             this.optionAnswer1.text = question.options[0]
             this.optionAnswer2.text = question.options[1]
             this.optionAnswer3.text = question.options[2]
+            // In questo when abbiamo dovuto settare il colore del testo delle opzioni sbagliate a nero, e quello
+            // di quella giusta verde perché quando il programma riciclava le viste degli elementi della lista
+            // aumentava le risposte verdi. Da 1 che doveva essere, ne metteva 2 o anche 3, perché "riciclava" la
+            // vista di quelle sopra.
             when(question.answer) {
                 0 -> {
                     this.optionAnswer1.setTextColor(Color.GREEN)
@@ -78,30 +74,30 @@ class QuestionAdapter(private var questionList: MutableList<Question>) : Recycle
                 }
             }
         }
-
         override fun onClick(v: View?) {
-                     val bundle = bundleOf("question" to this.questionText.text.toString())
-                      v?.findNavController()?.navigate(R.id.action_listQuestionsFragment_to_editQuestionFragment, bundle)
+            // Alla pressione dell'elemento della lista (qualsiasi punto al di fuori delle stringhe e del bottone per
+            // cancellare, compreso tra le due righe rosse) si apre il fragment che permette di modificare la domanda e
+            // aggiornare il database.
+            // La prima riga imposta i parametri da passare al nuovo fragment:
+            // x to y vuol dire che metti il valore y nella variabile x.
+            // La seconda riga, come sempre, naviga tra i due fragment.
+            val bundle = bundleOf("question" to this.questionText.text.toString())
+            v?.findNavController()?.navigate(R.id.action_listQuestionsFragment_to_editQuestionFragment, bundle)
         }
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
         val view = inflater.inflate(R.layout.question_details_layout,parent,false)
-       val holder= QuestionViewHolder(view)
-//   mmaxy40@gmail.com
-
-
+        //val holder= QuestionViewHolder(view) // Questo non considerarlo.
         return QuestionViewHolder(view)
     }
-
     override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
-
         val item = questionList[position]
-        holder.bindQuestion(this, item, position)
+        holder.bindQuestion(this, item, position) // Qui, a differenza del progetto di Riccardo del diario, abbiamo dovuto
+        // inserire due nuovi parametri "parent" e "position" per poter identificare l'elemento da cancellare (position) all'interno
+        // della nostra lista (this), non avendo gli id.
     }
-
     override fun getItemCount(): Int {
         return questionList.size
     }
